2daefb2c67aeb22ac9949ef3a8d33ff4506cb6f0
İsmail Sefa Esgün <isefaesgun@gmail.com>
2024-10-17 21:21:19 +0300

    modified js file

--- js/clock.js
+++ js/clock.js
@@ -10,16 +10,14 @@ function showTime() {
     // Tarih bilgilerini al
     const days = ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'];
     const dayName = days[now.getDay()]; // Haftanın gününü isme çevir
-    const date = now.getDate();
-    const month = (now.getMonth() + 1); // Aylar 0-11 arasında olduğu için +1 ekliyoruz
-    const year = now.getFullYear();
+    
 
     // Zaman ve tarih formatı
-    const currentTime = `${hours}:${minutes}:${seconds}`;
-    const currentDate = `${dayName}, ${date}.${month}.${year}`;
+    const currentTime = `${hours}:${minutes}:${seconds} ${dayName}`;
+    
 
     // Saat ve tarihi elemente yaz
-    clockElement.innerText = `${currentDate} - ${currentTime}`;
+    clockElement.innerText = currentTime;
 
     // Her saniyede bir güncellenmesi için
     setTimeout(showTime, 1000);
